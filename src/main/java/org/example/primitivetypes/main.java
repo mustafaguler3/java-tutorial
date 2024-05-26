a43207dcc5c4745761bd7eca41ac366b041d7cf6
mustafaguler3 <mustafa.glr@hotmail.com>
2024-05-26 13:05:29 +0300

    primitive types

---
+++ src/main/java/org/example/primitivetypes/main.java
@@ -0,0 +1,34 @@
+package org.example.primitivetypes;
+
+public class main {
+    public static void main(String[] args) {
+        byte a = 100;
+        short b = 30_000;
+        boolean flag = true;  // default value - false
+        char c = 78; // N
+        int i = 42;
+        double d = 45D;
+        float f = 47f;
+        long l = 67;
+
+        System.out.println(c);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
